f26082fb09d45e9a1ac5547e561195b8284df871
Jeremy Parker <jrobinparker@gmail.com>
2018-06-20 20:09:48 +0000

    automatically backed up by learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,7 @@
 class Author < ActiveRecord::Base
   
   validates :name, uniqueness: true 
-  validates :phone_number,  length: { is 20 }
+  validates :phone_number,  length: { is: 10 }
+  validates :title, presence: true 
+  
 end
