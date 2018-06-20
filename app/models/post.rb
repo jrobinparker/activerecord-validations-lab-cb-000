8ed80b82c9f607258a284835261a9ff350e215bf
Jeremy Parker <jrobinparker@gmail.com>
2018-06-20 21:10:27 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,16 @@
 class Post < ActiveRecord::Base
 
     validates :title, presence: :true
+    validate :clickbaity_title
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+  def clickbaity_title
+    if self.title.include?(["Won't Believe", "Secret", "Top 10", "Guess"])
+      errors.add(:title, "title is not clickbaity enough!")
+    end
+  end
 
 
 end
