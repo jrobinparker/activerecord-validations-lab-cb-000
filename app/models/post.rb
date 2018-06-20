5704dcc553581d91c3b28e0dd398a0e9dc22448d
Jeremy Parker <jrobinparker@gmail.com>
2018-06-20 20:23:29 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,8 @@ class Post < ActiveRecord::Base
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 
-  def validates_
+  def clickbait
+    clickbait_title = ["Won't Believe", "Secret", "Top 10", "Guess"]
+    if title.includes.any?
 
 end
