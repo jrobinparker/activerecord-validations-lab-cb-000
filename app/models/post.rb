e53555afc4124e76075db80adcf9dfec829c3c9c
Jeremy Parker <jrobinparker@gmail.com>
2018-06-20 20:51:29 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
 
-    validates_inclusion_of :title, { in: %w("Won't Believe") } 
+    validates_inclusion_of :title, { in: %w(Believe) } 
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
