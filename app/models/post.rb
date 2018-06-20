02099063f1aad0cfd7404e88e879fd1bf929a8d4
Jeremy Parker <jrobinparker@gmail.com>
2018-06-20 20:57:17 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,13 @@ class Post < ActiveRecord::Base
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 
+  def correct_sphare_types
+    if self.sphare.blank?
+      errors.add(:sphare, "sphare is blank/invalid")
+    elsif self.sphare.detect { |s| !(%w(Good Bad Neutral).include? s) }
+      errors.add(:sphare, "sphare is invalid")
+    end
+  end
+
+
 end
