8257b5e7e184de434f150a4f58fa32be5c3567d8
Jeremy Parker <jrobinparker@gmail.com>
2018-06-20 20:11:16 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ActiveRecord::Base
   
     validates :title, presence: true
-    validates :
+    validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
+    validates :category, 
   
 end
