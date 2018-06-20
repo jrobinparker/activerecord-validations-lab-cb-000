c08d8213141cf07b652b297b31a3e3809844318e
Jeremy Parker <jrobinparker@gmail.com>
2018-06-20 20:30:27 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ActiveRecord::Base
   
-    validates :title, presence: true, inclusion: { in: %w("Won't Believe" Secret "Top 10" Guess) }
+    validates :title, presence: true
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-
+    validates :title, 
 end
