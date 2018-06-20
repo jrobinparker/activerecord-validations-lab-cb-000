cf7cd1316c4a121afcaeb605463b82a038982878
Jeremy Parker <jrobinparker@gmail.com>
2018-06-20 20:37:47 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,7 @@ class Post < ActiveRecord::Base
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-    validates_inclusion_of :title,  in: %w("Won't Believe" Secret "Top 10" Guess)
-    
+
     def clickbaity?
       title.include? = ["Won't Believe", "Secret", "Top 10", "Guess"]
     end
